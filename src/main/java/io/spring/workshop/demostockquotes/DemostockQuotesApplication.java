e51f4d446c4cc68fc27b1e6bf0020e0e3d851943
Daniel David Kovacs <v-dakovacs@expedia.com>
2018-12-14 10:04:57 +0100

    init commit

---
+++ src/main/java/io/spring/workshop/demostockquotes/DemostockQuotesApplication.java
@@ -0,0 +1,14 @@
+package io.spring.workshop.demostockquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemostockQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemostockQuotesApplication.class, args);
+	}
+
+}
+

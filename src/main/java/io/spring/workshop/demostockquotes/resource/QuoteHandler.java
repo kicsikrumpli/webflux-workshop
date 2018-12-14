a4300d51151e6b06dde4197e8f4f3b0a3f69cd2f
Daniel David Kovacs <v-dakovacs@expedia.com>
2018-12-14 11:14:14 +0100

    functional hello world router and handler

--- src/main/java/io/spring/workshop/demostockquotes/resource/QuoteHandler.java
+++ src/main/java/io/spring/workshop/demostockquotes/resource/QuoteHandler.java
@@ -1,4 +1,22 @@
 package io.spring.workshop.demostockquotes.resource;
 
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
+
+@Component
 public class QuoteHandler {
+
+    public Mono<ServerResponse> hello(ServerRequest request) {
+        return ServerResponse
+                .ok()
+                .contentType(TEXT_PLAIN)
+                .syncBody("hello")
+                .log();
+    }
+
 }
--- src/main/java/io/spring/workshop/demostockquotes/resource/QuoteRouter.java
+++ src/main/java/io/spring/workshop/demostockquotes/resource/QuoteRouter.java
@@ -1,4 +1,25 @@
 package io.spring.workshop.demostockquotes.resource;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
 public class QuoteRouter {
+    @Autowired
+    private QuoteHandler handler;
+
+    @Bean
+    public RouterFunction<ServerResponse> helloRouterFunction() {
+        return RouterFunctions.route()
+                .GET("/hello-world", accept(MediaType.TEXT_PLAIN), handler::hello)
+                .build();
+    }
+
 }

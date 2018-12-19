201d8fcbfe7f9e3a93fb865c61e8a425ee55f54b
Daniel David Kovacs <v-dakovacs@expedia.com>
2018-12-19 15:42:45 +0100

    echo function and quote stream

--- src/main/java/io/spring/workshop/demostockquotes/resource/QuoteHandler.java
+++ src/main/java/io/spring/workshop/demostockquotes/resource/QuoteHandler.java
@@ -1,15 +1,29 @@
 package io.spring.workshop.demostockquotes.resource;
 
+import static org.springframework.http.MediaType.APPLICATION_STREAM_JSON;
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 
+import java.time.Duration;
+
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import io.spring.workshop.demostockquotes.domain.Quote;
+import io.spring.workshop.demostockquotes.generator.QuoteGenerator;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
 public class QuoteHandler {
+    private Flux<Quote> lotsOfQuotes;
+
+    public QuoteHandler(QuoteGenerator quoteGenerator) {
+        lotsOfQuotes = quoteGenerator
+                .fetchQuoteStream(Duration.ofMillis(200L))
+                .share();
+    }
 
     public Mono<ServerResponse> hello(ServerRequest request) {
         return ServerResponse
@@ -17,6 +31,19 @@ public class QuoteHandler {
                 .contentType(TEXT_PLAIN)
                 .syncBody("hello")
                 .log();
+    }
+
+    public Mono<ServerResponse> echo(ServerRequest request) {
+        return ServerResponse
+                .ok()
+                .body(request.bodyToMono(String.class), String.class);
+    }
+
+    public Mono<ServerResponse> streamQuotes(ServerRequest request) {
+        return ServerResponse
+                .ok()
+                .contentType(APPLICATION_STREAM_JSON)
+                .body(lotsOfQuotes, Quote.class);
     }
 
 }
